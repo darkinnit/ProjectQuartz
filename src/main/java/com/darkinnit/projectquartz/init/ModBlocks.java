6d2ea796e1c42f2856c6ccc8ee8b773f99ea1162
darkinnit <darkinnit@gmail.com>
2014-08-14 22:43:24 +0100

    Placeholder textures. Need to fix assigning them to sides

--- src/main/java/com/darkinnit/projectquartz/init/ModBlocks.java
+++ src/main/java/com/darkinnit/projectquartz/init/ModBlocks.java
@@ -2,6 +2,7 @@ package com.darkinnit.projectquartz.init;
 
 import com.darkinnit.projectquartz.block.BlockObserver;
 import com.darkinnit.projectquartz.block.BlockPQ;
+import com.darkinnit.projectquartz.block.BlockResponder;
 import com.darkinnit.projectquartz.reference.Reference;
 import cpw.mods.fml.common.registry.GameRegistry;
 
@@ -11,9 +12,11 @@ import cpw.mods.fml.common.registry.GameRegistry;
 @GameRegistry.ObjectHolder(Reference.MOD_ID)
 public class ModBlocks {
     public static final BlockPQ observer = new BlockObserver();
+    public static final BlockPQ responder = new BlockResponder();
 
     public static void init() {
         GameRegistry.registerBlock(observer, "observer");
+        GameRegistry.registerBlock(responder, "responder");
     }
 
 }
